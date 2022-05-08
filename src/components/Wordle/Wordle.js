1c981f938188016b196db711b2a4744797456cb7
LAPTOP-2GD2SQUR <gregory.saison@gmail.com>
2022-05-08 15:42:11 +0200

    ajout commentaire explicatif

--- src/components/Wordle/Wordle.js
+++ src/components/Wordle/Wordle.js
@@ -1,5 +1,5 @@
 // ----- IMPORT ----- //
-import { useEffect, useReducer } from "react"; // Hook permettant la création et la manipulation de l'état du composant
+import { useEffect, useReducer } from "react";
 import { Context } from "./../../helpers/context"; // Import d'un contexte crée
 import { answerReducer } from "../../helpers/answerReducer";
 
@@ -11,8 +11,11 @@ import Grid from "./../Grid";
 function Wordle() {
   // Je définis un état initial sur la valeur "" dans une variable answer
   // Et une méthode setAnswer me permettant de manipuler ultérieurement cette valeur définie
+  // useReducer permet l'utilisation de mon answerReducer paramétré
   const [answer, setAnswer] = useReducer(answerReducer, "");
 
+  // Logique de récupération de la valeur de answer
+  // Utilisation du hook useEffect
   useEffect(() => {
     const handleKeyDown = (event) => setAnswer(event.key);
 
